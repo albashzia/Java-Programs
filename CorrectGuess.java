e01f5ceff33f6c2636d7af6ce25b161499d7a4c6
albashzia <ahmedalbashzia@gmail.com>
2025-11-01 17:01:37 +0000

    CorrectGuess program to implement a simple guessing game

---
+++ CorrectGuess.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.util.Random;
+public class CorrectGuess 
+{
+    public static void main(String[] args) 
+    {
+        Random rand = new Random();
+        int randomNumber = rand.nextInt(2);
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a guess number (0,1), 0 for Heads and 1 for Tails : ");
+        int n = input.nextInt();
+        if (n == 0 || n == 1 )
+        {
+            if(randomNumber == n)
+            {
+                System.out.println("Correct Guess");
+            }
+            else if(randomNumber != n)
+            {
+                System.out.println("Wrong Guess");
+            }
+        }
+        else
+        {
+            System.out.println("Invalid Guess");
+        }
+    }
+}
