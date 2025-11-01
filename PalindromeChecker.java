89cda25ad4a402438c4db62aaaff776faf956b8b
albashzia <ahmedalbashzia@gmail.com>
2025-11-01 16:57:33 +0000

    Program to check if a three-digit integer is a palindrome

---
+++ PalindromeChecker.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class PalindromeChecker 
+{
+    public static void main(String[] args) 
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a three digit integer : ");
+        int num = input.nextInt();
+        int first = num/100;
+        int last = num%10;
+        if (first == last)
+            System.out.println(num+" is a palindrome");
+        else
+            System.out.println(num+" is not a palindrome");
+    }
+}
