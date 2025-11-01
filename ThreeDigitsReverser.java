39a9906a6341a90bfce801d80515e785de210e24
albashzia <ahmedalbashzia@gmail.com>
2025-11-01 13:25:37 +0000

    Add ThreeDigitsReverser class to reverse a three-digit integer and calculate its sum

---
+++ ThreeDigitsReverser.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class ThreeDigitsReverser 
+{
+    public static void main(String[] args) 
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a three digit integer : ");
+        int num = input.nextInt();
+        int d1 = num/100;
+        int d2 = (num/10)%10;
+        int d3 = num%10;
+        int reversed = (d3*100) + (d2*10) + (d1) ;
+        int sum = num + reversed;
+        System.out.println("The number entered is : "+num);
+        System.out.println("The reversed number is : "+reversed);
+        System.out.println("The sum is : "+sum);
+    }
+}
