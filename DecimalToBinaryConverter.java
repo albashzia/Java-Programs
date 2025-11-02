09c0b96eb8d0986a3755b7b84ae14efaade7d562
albashzia <ahmedalbashzia@gmail.com>
2025-11-02 13:55:43 +0000

    Decimal to Binary Converter program

---
+++ DecimalToBinaryConverter.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class DecimalToBinaryConverter
+{
+    public static void main(String[] args) 
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a decimal integer: ");
+        int number = input.nextInt();
+        String binary = ""; 
+        if (number == 0) 
+        {
+            binary = "0";
+        }
+        else 
+        {
+            while (number > 0) 
+            {
+                int remainder = number % 2;    
+                binary = remainder + binary;   
+                number = number / 2;           
+            }
+        }
+        System.out.println("Binary value: " + binary);
+    }
+}
