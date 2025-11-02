b0271517765487016c17d05e0c7f06bde0d543a5
albashzia <ahmedalbashzia@gmail.com>
2025-11-02 02:18:37 +0000

    Program to find and print perfect numbers less than 10,000

---
+++ FindPerfectNumbers.java
@@ -0,0 +1,21 @@
+public class FindPerfectNumbers 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Perfect numbers less than 10,000 are:");
+        for (int n = 1; n < 10000; n++) {
+            int sum = 0;
+            for (int i = 1; i <= n / 2; i++) 
+            {
+                if (n % i == 0) 
+                {
+                    sum = sum + i;
+                }
+            }
+            if (sum == n) 
+            {
+                System.out.println(n);
+            }
+        }
+    }
+}
