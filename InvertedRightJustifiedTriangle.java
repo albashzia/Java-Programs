3520e1983caf1a1c75d21d890cbe64e293d7f82f
albashzia <ahmedalbashzia@gmail.com>
2025-11-02 02:11:48 +0000

    Program to print an inverted right-justified triangle pattern using numbers

---
+++ InvertedRightJustifiedTriangle.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class InvertedRightJustifiedTriangle 
+{
+    public static void main(String[] args) 
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number of lines : ");
+        int num = input.nextInt();
+        for(int i = num; i >= 1; i--)
+        {
+            for(int j = 1; j <= num - i; j++ )
+            {
+                System.out.print("  "); 
+            }
+            for (int j = 1; j <= i; j++) 
+            {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
