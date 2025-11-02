70c3021cea0faf282685f2f964525657cded42e4
albashzia <ahmedalbashzia@gmail.com>
2025-11-02 02:06:50 +0000

    Program to print a right triangle pattern using numbers

---
+++ RightTrianglePattern.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class RightTrianglePattern
+{
+    public static void main(String[] args) 
+    {
+         Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number of lines : ");
+        int num = input.nextInt();
+        for(int i = 1; i <= num; i++)
+        {
+            for (int j = 1; j <= i; j++) 
+            {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
