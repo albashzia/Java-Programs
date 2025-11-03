b4c43d75761b9a041e062970ea3bbf3fab02a63c
albashzia <ahmedalbashzia@gmail.com>
2025-11-03 04:33:07 +0000

    Program to convert minutes to hours and minutes

---
+++ TimeConverter.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class TimeConverter 
+{
+    public static void main(String[] args) 
+    {
+        int N,H,M;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the minutes passed till midnight : ");
+        N = input.nextInt();
+        //System.out.println(N);
+        H = N/60;
+        M = N%60;
+        System.out.println(H+" "+M);
+    }
+}
