c55c1dff4969dc29e9360eb0324c6574a32c4fe6
albashzia <ahmedalbashzia@gmail.com>
2025-11-03 04:29:51 +0000

    Program to calculate total desks needed based on student input

---
+++ DeskRequirementCalculator.java
@@ -0,0 +1,22 @@
+import static java.lang.Math.*;
+import java.util.Scanner;
+public class DeskRequirementCalculator
+{
+    public static void main(String[] args) 
+    {
+        Scanner input = new Scanner(System.in);
+        int a,b,c, desks;
+        System.out.print("Enter the number of Students in class 1 : ");
+        a = input.nextInt();
+        System.out.print("Enter the number of Students in class 2 : ");
+        b = input.nextInt();
+        System.out.print("Enter the number of Students in class 3 : ");
+        c = input.nextInt();
+        int desksA = (a/2)+(a%2);
+        int desksB = (b/2)+(b%2);
+        int desksC = (c/2)+(c%2);
+        desks = desksA + desksB + desksC ;
+        System.out.println("The total number of desks to be bought is "+desks);
+        input.close();
+    }
+}
