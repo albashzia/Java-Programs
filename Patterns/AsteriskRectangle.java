067371f8b8003d1b16e88377066af7324884cd1f
albashzia <ahmedalbashzia@gmail.com>
2025-11-05 11:05:36 +0000

    Add hollow asterisk rectangle pattern

---
+++ Patterns/AsteriskRectangle.java
@@ -0,0 +1,26 @@
+package Patterns;
+import java.util.Scanner;
+public class AsteriskRectangle
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter number of rows: ");
+        int rows = input.nextInt();
+        System.out.print("Enter number of columns: ");
+        int columns = input.nextInt();
+        for(int i = 1; i <= rows; i++)
+        {
+            for(int j = 1; j<=columns;j++)
+            {
+                if( i==1 || i==rows || j==1 || j==columns )
+                {
+                    System.out.print("*");
+                }
+                else
+                    System.out.print(" ");
+            }
+            System.out.println("");
+        }
+    }
+}
