55c9a87c619ddfda0cb22cfe1c441417fd20d5a3
albashzia <ahmedalbashzia@gmail.com>
2025-11-06 01:46:10 +0000

    Calculates the sum of N integers provided by the user.

---
+++ CalculateNSum.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class CalculateNSum
+{
+    public static void main(String[] args) 
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number of integers to calculate sum : ");
+        int N = input.nextInt();
+        int sum = 0;
+        System.out.println("Enter the numbers one by one : ");
+        for(int i = 0; i < N; i++)
+        {
+            int n = input.nextInt();
+            sum = sum+n;
+        }
+        System.out.println("The sum is : "+sum);
+    }
+}
