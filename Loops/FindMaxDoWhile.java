65b49ad97fef01ad17e897397cd1c082646f8c99
albashzia <ahmedalbashzia@gmail.com>
2025-11-07 09:38:44 +0000

    Find maximum number using a do-while loop until zero.

---
+++ Loops/FindMaxDoWhile.java
@@ -0,0 +1,20 @@
+package Loops;
+import java.util.Scanner; 
+public class FindMaxDoWhile
+{ 
+    public static void main(String[] args) 
+    { 
+        Scanner input = new Scanner(System.in); 
+        int number, max; 
+        System.out.println("Enter the numbers, enter 0 to exit. ");
+        number = input.nextInt();  
+        max = number;
+        do 
+        { 
+        number = input.nextInt(); 
+        if (number > max) 
+        max = number; 
+        }while (number != 0); 
+        System.out.println("max is " + max+" and number " + number); 
+    } 
+}
