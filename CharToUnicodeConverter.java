4b0871b80a599e98ba7a39b35fb37fa16dfeeffa
albashzia <ahmedalbashzia@gmail.com>
2025-11-08 10:41:24 +0000

    Convert input character to its equivalent Unicode (integer) value

---
+++ CharToUnicodeConverter.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class CharToUnicodeConverter
+{
+    public static void main(String[] args) 
+    {
+        Scanner input = new Scanner(System.in);
+        char ch;
+        System.out.print("Enter a character : ");
+        ch = input.next().charAt(0);
+        int num = (int)ch;
+        System.out.println("The Unicode for the character "+ch+" is "+num);
+    }
+}
