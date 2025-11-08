e86b342b703a38bdbb78fc87ec159a4efcc9b798
albashzia <ahmedalbashzia@gmail.com>
2025-11-08 09:48:06 +0000

    Analyze day type based on input temperature ranges

---
+++ TemperatureDayAnalyzer.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class TemperatureDayAnalyzer 
+{
+    public static void main(String[] args) 
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the temperature : ");
+        double temperature = input.nextDouble();
+        if (temperature > 35 )
+        {
+            System.out.println("Hot day");
+        }
+        else if (temperature >= 25)
+        {
+            System.out.println("Pleasant day");
+        }
+        else if (temperature <= 24 && temperature >= 18)
+        {
+            System.out.println("Cool day");
+        }
+        else if(temperature < 18)
+        {
+            System.out.println("Cold day");
+        }
+    }
+}
