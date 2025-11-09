8f658aa680701d4570a35f9f65f30cbb6740f94d
albashzia <ahmedalbashzia@gmail.com>
2025-11-09 15:57:48 +0000

    Find the smaller number using a simple if-else structure

---
+++ FindSmallerNumber.java
@@ -0,0 +1,17 @@
+public class FindSmallerNumber 
+{
+    public static void main(String[] args) 
+    {
+        int x,y;
+        x = 15;
+        y = 10;
+        if(x<y)
+        {
+            System.out.println("The smaller number is : "+x);
+        }
+        else
+        {
+            System.out.println("The smaller number is : "+y);
+        }
+    }
+}
