89ebe6f63f47a8320f6fb03eee70edbd96595861
albashzia <ahmedalbashzia@gmail.com>
2025-11-09 15:31:56 +0000

    Check if an input string is a palindrome using iteration.

---
+++ StringPalindromeChecker.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class StringPalindromeChecker 
+{
+    public static void main(String[] args) 
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = input.nextLine();
+        String reverse = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reverse = reverse + str.charAt(i);
+        }
+        if (str.equals(reverse))
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not a palindrome");
+
+        input.close();
+    }
+}
