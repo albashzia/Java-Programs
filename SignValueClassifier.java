0e96b5aa8317f5746eb796af246679b690f8e2da
albashzia <ahmedalbashzia@gmail.com>
2025-11-09 16:06:26 +0000

    Classify number as 1, 0, or -1 based on its sign

---
+++ SignValueClassifier.java
@@ -0,0 +1,19 @@
+public class SignValueClassifier
+{
+    public static void main(String[] args) 
+    {
+        int x = 10;
+        if(x > 0)
+        {
+            System.out.println("1");
+        }
+        else if(x < 0)
+        {
+            System.out.println("-1");
+        }
+            else if(x == 0)
+        {
+            System.out.println("0");
+        }
+    }
+}
