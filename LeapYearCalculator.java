e456e555276d75f5cfab8a08dc82a9625a97869e
albashzia <ahmedalbashzia@gmail.com>
2025-11-09 15:53:16 +0000

    Closing input scanner to ensure proper resource management

--- LeapYearCalculator.java
+++ LeapYearCalculator.java
@@ -8,5 +8,6 @@ public static void main(String[] args)
         int year = input.nextInt(); 
         boolean isLeapYear = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0); 
         System.out.println(year + " is a leap year? " + isLeapYear); 
+        input.close();
     }  
 } 
