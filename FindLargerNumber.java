b56b798d6e62906e4aea14eafccf80364c60eece
albashzia <ahmedalbashzia@gmail.com>
2025-11-09 15:59:19 +0000

    Find the larger number using a simple if-else structure

---
+++ FindLargerNumber.java
@@ -0,0 +1,17 @@
+public class FindLargerNumber 
+{
+    public static void main(String[] args) 
+    {
+        int x,y;
+        x = 5;
+        y = 10;
+        if(x>y)
+        {
+            System.out.println("The larger number is : "+x);
+        }
+        else
+        {
+            System.out.println("The larger number is : "+y);
+        }
+    }
+}
