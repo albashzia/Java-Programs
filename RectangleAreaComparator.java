1f87d808177cf6606d1a4b4851812e2d9da7abe1
albashzia <ahmedalbashzia@gmail.com>
2025-11-09 16:10:17 +0000

    Compare and output which of two rectangles has a larger area

---
+++ RectangleAreaComparator.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class RectangleAreaComparator 
+{
+    public static void main(String[] args) 
+    {
+        Scanner input = new Scanner(System.in);
+        double lengthOne, lengthTwo, widthOne, widthTwo, areaOne, areaTwo;
+        System.out.print("Enter the length of first rectangle : ");
+        lengthOne = input.nextDouble();
+        System.out.print("Enter the width of first rectangle : ");
+        widthOne = input.nextDouble();
+        areaOne = lengthOne*widthOne;
+        
+        System.out.print("Enter the length of second rectangle : ");
+        lengthTwo = input.nextDouble();
+        System.out.print("Enter the width of second rectangle : ");
+        widthTwo = input.nextDouble();
+        areaTwo = lengthTwo*widthTwo;
+        
+        System.out.println("The area of first rectangle is : "+areaOne);
+        System.out.println("The area of second rectangle is : "+areaTwo);
+        
+        if ( areaOne > areaTwo)
+        {
+            System.out.println("First Rectangle has larger area");
+        }
+        else if ( areaTwo > areaOne)
+        {
+            System.out.println("Second Rectangle has larger area");
+        }
+        else
+        {
+            System.out.println("Both rectangles have equal area");
+        }
+    }
+}
