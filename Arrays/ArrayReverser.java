e4f927cbd382443d6a2d5909c4a36a1ea2975e28
albashzia <ahmedalbashzia@gmail.com>
2025-11-11 15:36:07 +0000

    Add program to reverse the order of elements in an array.

---
+++ Arrays/ArrayReverser.java
@@ -0,0 +1,23 @@
+package Arrays;
+import java.util.Scanner;
+public class ArrayReverser 
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number of elements in the array: ");
+        int n = input.nextInt();
+        int[] array = new int[n];
+        System.out.println("Enter the elements of the array: ");
+        for (int i = 0; i < n; i++) 
+        {
+            array[i] = input.nextInt();
+        }
+        System.out.println("Reversed array: ");
+        for (int i = n - 1; i >= 0; i--) 
+        {
+            System.out.println(array[i]);  
+        }
+        input.close(); 
+    }
+}
