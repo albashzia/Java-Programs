4c262d2adeca74675649e48d9b603d76a45df03d
albashzia <ahmedalbashzia@gmail.com>
2025-11-11 15:44:44 +0000

    Add PassFailCounter program to count passing and failing students based on mark threshold

---
+++ Arrays/PassFailCounter.java
@@ -0,0 +1,33 @@
+package Arrays;
+import java.util.Scanner;
+public class PassFailCounter 
+{
+    public static void main(String[] args) 
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number of students: ");
+        int n = input.nextInt();
+        int[] scores = new int[n];
+        System.out.println("Enter the scores of the students: ");
+        for (int i = 0; i < n; i++) 
+        {
+            scores[i] = input.nextInt();
+        }
+        int passCount = 0;
+        int failCount = 0;
+        for (int i = 0; i < n; i++) 
+        {
+            if (scores[i] >= 50) 
+            {
+                passCount++;
+            } 
+            else 
+            {
+                failCount++;
+            }
+        }
+        System.out.println("Number of students who passed: " + passCount);
+        System.out.println("Number of students who failed: " + failCount);
+        input.close();
+    }
+}
