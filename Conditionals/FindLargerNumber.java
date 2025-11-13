b7ab83f8b1b2144dc990bc79389bc06ace8a5458
albashzia <ahmedalbashzia@gmail.com>
2025-11-13 18:00:05 +0000

    Organizing Programs under different folders

--- Conditionals/FindLargerNumber.java
+++ Conditionals/FindLargerNumber.java
@@ -1,3 +1,4 @@
+package Conditionals;
 public class FindLargerNumber 
 {
     public static void main(String[] args) 
--- Conditionals/FindSmallerNumber.java
+++ Conditionals/FindSmallerNumber.java
@@ -1,3 +1,4 @@
+package Conditionals;
 public class FindSmallerNumber 
 {
     public static void main(String[] args) 
