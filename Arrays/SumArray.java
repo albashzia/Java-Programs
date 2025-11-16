da753188dcc34be599fdf0864d456128d3102531
albashzia <ahmedalbashzia@gmail.com>
2025-11-16 18:12:58 +0000

    Adding SumArray program to calculate and display the sum of 5 elements inserted in the array using a method

---
+++ Arrays/SumArray.java
@@ -0,0 +1,26 @@
+package Arrays;
+import java.util.Scanner;
+public class SumArray 
+{
+    public static void main(String[] args) 
+    {
+        Scanner input = new Scanner(System.in);
+        int[] arr = new int[5];
+        System.out.println("Enter array elements: ");
+        for(int i = 0; i < 5; i++)
+        {
+            arr[i] = input.nextInt();
+        }
+        System.out.println("The sum of array elements is "+sum(arr));
+        input.close();
+    }
+    static int sum(int[] arr)
+    {
+        int sum = 0;
+        for(int x:arr)
+        {
+            sum +=x;
+        }
+        return sum;
+    }
+}
