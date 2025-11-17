eca9cb267043edfd32b6b262af85bbebb71b4c88
albashzia <ahmedalbashzia@gmail.com>
2025-11-17 05:30:47 +0000

    Adding ReversedArrayCopier program which takes an array of 10 elements as an input from the user copies it to another array in reverse order of the inputted array

---
+++ Arrays/ReversedArrayCopier.java
@@ -0,0 +1,33 @@
+package Arrays;
+import java.util.Scanner;
+public class ReversedArrayCopier 
+{
+    public static void main(String[] args) 
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println("-------------------------------------------");
+        System.out.println("            Reversed Array Copier");
+        System.out.println("-------------------------------------------");
+        System.out.println("");
+        int array[] = new int[10];
+        int copiedArray[] = new int[10];
+        for(int i = 0; i < array.length; i++)
+        {
+            System.out.print("Enter the element no "+(i+1)+" : ");
+            array[i] = input.nextInt();
+        }
+        int j = 0;
+        for(int i = array.length-1; i>=0; i--)
+        {
+                copiedArray[j] = array[i];
+                j++;
+        }
+        System.out.println("    ");
+        System.out.println("The copied array is as follow :\n");
+        for (int i = 0; i < 10; i++) 
+        {
+            System.out.println("The elements at index "+i+" is : " +copiedArray[i]);
+        }
+        input.close();
+    }
+}
