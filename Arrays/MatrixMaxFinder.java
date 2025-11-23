eb45e8fab3fcf4d2e02fc18358038555cc4656ec
albashzia <ahmedalbashzia@gmail.com>
2025-11-23 07:19:37 +0000

    Removing package

--- Arrays/MatrixMaxFinder.java
+++ Arrays/MatrixMaxFinder.java
@@ -1,5 +1,3 @@
-package Arrays;
-
 public class MatrixMaxFinder {
     public static void main(String[] args) {
         int[][] matrix = {
--- Arrays/MatrixPrinter.java
+++ Arrays/MatrixPrinter.java
@@ -1,5 +1,3 @@
-package Arrays;
-
 public class MatrixPrinter 
 {
     public static void main(String[] args) 
