a58e41e3bbc037193ad7a5c54fabf02668bfb506
albashzia <ahmedalbashzia@gmail.com>
2025-11-23 07:15:40 +0000

    Add program to detect four consecutive identical integers in an array

---
+++ Arrays/ConsecutiveFourChecker.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+public class ConsecutiveFourChecker 
+{
+    public static void main(String[] args) 
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter number of elements: ");
+        int n = input.nextInt();
+        int values[] = new int[n];
+        System.out.println("Enter the "+n+" values for the array: ");
+        for(int i = 0; i < n; i++)
+        {
+            values[i] = input.nextInt();
+        }
+        if (isConsecutiveFour(values)==true)
+        {
+            System.out.println("The list has consecutive fours");
+        }
+        else
+        {
+            System.out.println("The list has no consecutive fours");
+        }
+        input.close();
+    }
+    public static boolean isConsecutiveFour(int[] values)
+    {
+        int counter = 1;
+        for(int i = 1; i < values.length; i++)
+        {
+            if(values[i]==values[i-1])
+            {
+                counter ++;
+            }  
+            else
+            {
+                counter = 1;
+            }
+            if(counter==4)
+            {
+                return  true;
+            }
+        }
+        return false;
+    }
+}
