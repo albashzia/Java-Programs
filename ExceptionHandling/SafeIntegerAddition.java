59a7d79fa1a3e84b7ff5ec4c1be6e07bacd1813e
albashzia <ahmedalbashzia@gmail.com>
2025-12-01 19:51:25 +0000

    Implement SafeIntegerAddition with loop-based InputMismatch recovery.

---
+++ ExceptionHandling/SafeIntegerAddition.java
@@ -0,0 +1,29 @@
+package ExceptionHandling;
+import java.util.*;
+public class SafeIntegerAddition 
+{
+    public static void main(String[] args) 
+    {
+        Scanner input = new Scanner(System.in);
+        int a,b;
+        boolean validInput = false;
+        while(!validInput)
+        {
+            try
+            {
+                System.out.print("Enter 1st value: ");
+                a = input.nextInt();
+                System.out.print("Enter 2nd value: ");
+                b = input.nextInt();
+                System.out.println("The sum is "+(a+b));
+                validInput = true;
+            }
+            catch(InputMismatchException e1)
+            {
+                System.out.println("You have entered an invalid input, type integers only");
+                input.nextLine();
+            }
+        }
+        input.close();
+    }
+}
