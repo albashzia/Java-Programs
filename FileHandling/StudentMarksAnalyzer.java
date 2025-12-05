924ca36782189f7842f1ff3f0f20b30e3b67c67d
albashzia <ahmedalbashzia@gmail.com>
2025-12-05 17:06:52 +0000

    Adds functionality to read student records (roll number, name, marks) from 'students.txt' using Scanner. Iterates through the file to count and display the total number of students whose marks are strictly greater than 50.

---
+++ FileHandling/StudentMarksAnalyzer.java
@@ -0,0 +1,29 @@
+import java.io.File;
+import java.util.Scanner;
+public class StudentMarksAnalyzer 
+{
+    public static void main(String[] args) 
+    {
+        int count = 0;
+        try
+        {
+            File file = new File("D:\\Programming\\Java\\Programs\\FileHandling\\students.txt");
+            Scanner input = new Scanner(file);
+            while(input.hasNext())
+            {
+                int rno = input.nextInt();
+                String name = input.next();
+                int marks = input.nextInt();
+                if(marks > 50)
+                {
+                    count++;
+                }
+            }
+            System.out.println("The number of students with marks greater than 50 in file "+file.getName()+" are "+count);
+        }
+        catch(Exception e)
+        {
+            System.out.println(e.toString());
+        }
+    }
+}
