5bc3424708de495b0fa95c85d761b7ade9dd1e79
Ahmad Albash Zia <ahmedalbashzia@gmail.com>
2025-12-07 15:12:52 +0500

    Applies the temp file pattern to filter and retain only student records where marks are 50 or above.

---
+++ FileHandling/RecordFilter.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+public class RecordFilter 
+{
+    public static void main(String[] args) 
+    {
+        try
+        {   
+            File original = new File("D:\\Programming\\Java\\Programs\\FileHandling\\students.txt");
+            File temp = new File("D:\\Programming\\Java\\Programs\\FileHandling\\temp.txt");
+
+            Scanner input = new Scanner(original);
+            PrintStream ps = new PrintStream(temp); 
+
+            while(input.hasNext())
+            {
+                int rno = input.nextInt();
+                input.nextLine();                
+                String name = input.nextLine(); 
+                int marks = input.nextInt();
+
+                if(marks >= 50)
+                {
+                    ps.println(rno);
+                    ps.println(name);
+                    ps.println(marks);
+                }             
+            }
+
+            original.delete();
+            temp.renameTo(original);
+
+            System.out.println("File Update Successful");
+        }
+        catch(Exception e )
+        {
+            System.out.println(e.toString());
+        }
+    }
+}
