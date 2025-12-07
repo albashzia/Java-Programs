d59b7cb716e29102cac364301bb9fb7414ea9d6c
Ahmad Albash Zia <ahmedalbashzia@gmail.com>
2025-12-08 00:22:51 +0500

    Implement KeywordCounter for 'java' in book file

---
+++ FileHandling/KeywordCounter.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+public class KeywordCounter {
+    public static void main(String[] args) 
+    {
+        int count = 0;
+        try 
+        {
+            File file = new File("D:/Programming/Java/Programs/LabAssignment04/book.txt");
+            Scanner sc = new Scanner(file);
+            while (sc.hasNext()) 
+            {
+                String w = sc.next().toLowerCase();
+                if (w.equals("java")) 
+                {
+                    count++;
+                }
+            }
+            sc.close();
+            System.out.println(count);
+        } 
+        catch (Exception e) 
+        {
+            System.out.println("Error");
+        }
+    }
+}
