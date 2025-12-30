3050b176f0b4d82b74a292d17580ffa5b0aa6e47
albashzia <ahmedalbashzia@gmail.com>
2025-12-30 13:37:25 +0000

    Documenting the code

--- Recursion/CompoundInterestCalculator.java
+++ Recursion/CompoundInterestCalculator.java
@@ -3,13 +3,13 @@ package Recursion;
 public class CompoundInterestCalculator {
     public static double compCalc(double m, int t)
     {
-        if(t==0)
+        if(t==0) // base case
             return m;
         return 1.005 * compCalc(m,t-1);
     }
     public static void main(String[] args) 
     {
-        double result = compCalc(1000,3);
+        double result = compCalc(1000,3); // call to recursive function
         System.out.printf("Result : %.2f%n",result);
     }
 }
