7569add6d49eb51c72dc6915667a447da26146b7
albashzia <ahmedalbashzia@gmail.com>
2025-12-30 13:36:25 +0000

    Commenting the code

--- Recursion/ArrayReverser.java
+++ Recursion/ArrayReverser.java
@@ -3,7 +3,7 @@ package Recursion;
 public class ArrayReverser {
     public static void revArray(int[] arr, int index)
     {
-        if(index<0)
+        if(index<0) // base case
             return;
         System.out.println(arr[index]);
         revArray(arr, index-1);
@@ -11,6 +11,6 @@ public class ArrayReverser {
     public static void main(String[] args) {
         int[] array = {10, 20, 30, 40, 50};
         System.out.println("Reversed array elements:");
-        revArray(array, array.length - 1);
+        revArray(array, array.length - 1); // call to recursive function
     }
 }
