9a6fef2c02d77d85762353fe8c618046b3331fce
albashzia <ahmedalbashzia@gmail.com>
2026-01-13 09:57:24 +0000

    Handling exceptions in ArrayMerger.java

--- Arrays/ArrayMerger.java
+++ Arrays/ArrayMerger.java
@@ -1,38 +1,36 @@
 import java.util.Scanner;
-public class ArrayMerger
-{
-    public static void main(String[] args) 
-    {
+
+public class ArrayMerger {
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter the number of elements in list 1 : ");
-        int n1 = input.nextInt();
-        System.out.print("Enter the number of elements in list 2 : ");
-        int n2 = input.nextInt();
-        int[] list1 = new int[n1];
-        int[] list2 = new int[n2];
-        int mergedList[] = new int[n1+n2];
-        System.out.println("Enter the "+n1+" elements for list 1: ");
-        for(int i = 0 ; i < n1; i ++)
-        {
-            list1[i] = input.nextInt();
-        }
-        System.out.println("Enter the "+n2+" elements for list 2: ");
-        for(int j = 0 ; j < n2; j ++)
-        {
-            list2[j] = input.nextInt();
-        }
-        for(int k = 0; k < n1; k++)
-        {
-            mergedList[k] = list1[k];
-        }
-        for(int m = 0; m < n2 ;m++)
-        {
-            mergedList[n1+m] = list2[m];
-        }
-        System.out.print("The merged list is : ");
-        for(int l = 0; l < mergedList.length;l++)
-        {
-            System.out.print(mergedList[l]+" ");
+        try {
+            System.out.print("Enter the number of elements in list 1 : ");
+            int n1 = input.nextInt();
+            System.out.print("Enter the number of elements in list 2 : ");
+            int n2 = input.nextInt();
+            int[] list1 = new int[n1];
+            int[] list2 = new int[n2];
+            int mergedList[] = new int[n1 + n2];
+            System.out.println("Enter the " + n1 + " elements for list 1: ");
+            for (int i = 0; i < n1; i++) {
+                list1[i] = input.nextInt();
+            }
+            System.out.println("Enter the " + n2 + " elements for list 2: ");
+            for (int j = 0; j < n2; j++) {
+                list2[j] = input.nextInt();
+            }
+            for (int k = 0; k < n1; k++) {
+                mergedList[k] = list1[k];
+            }
+            for (int m = 0; m < n2; m++) {
+                mergedList[n1 + m] = list2[m];
+            }
+            System.out.print("The merged list is : ");
+            for (int l = 0; l < mergedList.length; l++) {
+                System.out.print(mergedList[l] + " ");
+            }
+        } catch (Exception e) {
+            System.out.println("Invalid input. Please enter integers only.");
         }
         input.close();
     }
