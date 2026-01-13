d646df3c648082b01014d3ccd35f0a870f438031
albashzia <ahmedalbashzia@gmail.com>
2026-01-13 09:59:40 +0000

    Handling exceptions in ArrayReverser.java

--- Arrays/ArrayReverser.java
+++ Arrays/ArrayReverser.java
@@ -1,22 +1,22 @@
 import java.util.Scanner;
-public class ArrayReverser 
-{
-    public static void main(String[] args)
-    {
+public class ArrayReverser {
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter the number of elements in the array: ");
-        int n = input.nextInt();
-        int[] array = new int[n];
-        System.out.println("Enter the elements of the array: ");
-        for (int i = 0; i < n; i++) 
-        {
-            array[i] = input.nextInt();
+        try {
+            System.out.println("Enter the number of elements in the array: ");
+            int n = input.nextInt();
+            int[] array = new int[n];
+            System.out.println("Enter the elements of the array: ");
+            for (int i = 0; i < n; i++) {
+                array[i] = input.nextInt();
+            }
+            System.out.println("Reversed array: ");
+            for (int i = n - 1; i >= 0; i--) {
+                System.out.println(array[i]);
+            }
+        } catch (Exception e) {
+            System.out.println("Invalid input. Please enter integers only.");
         }
-        System.out.println("Reversed array: ");
-        for (int i = n - 1; i >= 0; i--) 
-        {
-            System.out.println(array[i]);  
-        }
-        input.close(); 
+        input.close();
     }
 }
