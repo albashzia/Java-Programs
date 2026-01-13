efbac7d03421a025bafee1821be06a98b7b15194
albashzia <ahmedalbashzia@gmail.com>
2026-01-13 10:02:21 +0000

    Adding try-catch blocks in ArraySortCheckerMethod.java

--- Arrays/ArraySortCheckerMethod.java
+++ Arrays/ArraySortCheckerMethod.java
@@ -1,59 +1,50 @@
 import java.util.Scanner;
-public class ArraySortCheckerMethod 
-{
-    public static void main(String[] args) 
-    {
+
+public class ArraySortCheckerMethod {
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter the number of elements to be inserted in the array: ");
-        int n = input.nextInt();
-        int[] numbers = new int[n];
-        System.out.println("Enter array elements: ");
-        for(int i = 0; i < n; i++)
-        {
-            numbers[i] = input.nextInt();
-        }
-        if (isSorted(numbers)==true)
-        {
-            System.out.println("Sorted");
-            for(int j = 0; j < n; j++)
-            {
-                System.out.print(numbers[j]+" ");
+        try {
+            System.out.print("Enter the number of elements to be inserted in the array: ");
+            int n = input.nextInt();
+            int[] numbers = new int[n];
+            System.out.println("Enter array elements: ");
+            for (int i = 0; i < n; i++) {
+                numbers[i] = input.nextInt();
             }
-            System.out.println();
-        }
-        else
-            if(isSorted(numbers)==false)
-            {
+            if (isSorted(numbers) == true) {
+                System.out.println("Sorted");
+                for (int j = 0; j < n; j++) {
+                    System.out.print(numbers[j] + " ");
+                }
+                System.out.println();
+            } else if (isSorted(numbers) == false) {
                 System.out.println("Not sorted");
                 System.out.println("The sorted array is");
-                for (int i = 0; i < n-1; i++) 
-                {
-                    for (int j = i+1; j < n; j++) 
-                    {
-                        if (numbers[j] < numbers[i]) 
-                        {
+                for (int i = 0; i < n - 1; i++) {
+                    for (int j = i + 1; j < n; j++) {
+                        if (numbers[j] < numbers[i]) {
                             int temp = numbers[i];
                             numbers[i] = numbers[j];
                             numbers[j] = temp;
                         }
-                    }   
+                    }
                 }
-                for(int j = 0; j < n; j++)
-                {
+                for (int j = 0; j < n; j++) {
                     System.out.print(numbers[j] + " ");
                 }
-            System.out.println();
+                System.out.println();
             }
+        } catch (Exception e) {
+            System.out.println("Invalid input. Please enter integers only.");
+        }
+
         input.close();
     }
-    
-    public static boolean isSorted(int[] list)
-    {
+
+    public static boolean isSorted(int[] list) {
         boolean sorted = true;
-        for(int i = 0; i < list.length-1; i++)
-        {
-            if(list[i]>list[i+1])
-            {
+        for (int i = 0; i < list.length - 1; i++) {
+            if (list[i] > list[i + 1]) {
                 sorted = false;
             }
         }
