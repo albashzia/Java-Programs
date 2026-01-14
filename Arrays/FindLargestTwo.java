2af47875172189b56d2863db13500abbc7b56ad6
albashzia <ahmedalbashzia@gmail.com>
2026-01-14 16:37:05 +0000

    Add exception handling for user input in FindLargestTwo.java

--- Arrays/FindLargestTwo.java
+++ Arrays/FindLargestTwo.java
@@ -1,38 +1,38 @@
 import java.util.Scanner;
-public class FindLargestTwo 
-{
-    public static void main(String[] args) 
-    {
+
+public class FindLargestTwo {
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int[] array = new int[10];
-        System.out.println("Enter array elements: ");
-        for(int i = 0; i < 10; i++)
-        {
-            array[i] = input.nextInt();
+        try {
+            System.out.println("Enter array elements: ");
+            for (int i = 0; i < 10; i++) {
+                array[i] = input.nextInt();
+            }
+        } catch (Exception e) {
+            System.out.println("Invalid input. Please enter integers only.");
         }
+        
         int largest = array[0];
-        for(int i = 0; i<10; i++)
-        {
-            if (array[i] > largest)
-            {
+        for (int i = 0; i < 10; i++) {
+            if (array[i] > largest) {
                 largest = array[i];
             }
         }
-        
+
         int largest2;
-        if(array[0]==largest)
+        if (array[0] == largest)
             largest2 = array[1];
-        else 
+        else
             largest2 = array[0];
-        for(int i = 0; i<10; i++)
-        {
-            if(array[i]!=largest)
-                if(array[i]>largest2)
-                    largest2=array[i];
+        for (int i = 0; i < 10; i++) {
+            if (array[i] != largest)
+                if (array[i] > largest2)
+                    largest2 = array[i];
         }
-        
-        System.out.println("The largest number is "+largest);
-        System.out.println("The second largest number is "+largest2);
+
+        System.out.println("The largest number is " + largest);
+        System.out.println("The second largest number is " + largest2);
         input.close();
     }
 }
