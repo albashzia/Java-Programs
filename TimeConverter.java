d131aa1f4b8c93e203c8c98b233557f0f3c1ded7
albashzia <ahmedalbashzia@gmail.com>
2026-01-25 23:09:44 +0500

    Enhancing readability of the code

--- TimeConverter.java
+++ TimeConverter.java
@@ -4,13 +4,13 @@ public class TimeConverter
     public static void main(String[] args) 
     {
         int N,H,M;
-        Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in); // creating scanner object
         System.out.println("Enter the minutes passed till midnight : ");
-        N = input.nextInt();
+        N = input.nextInt(); // taking input from user
         //System.out.println(N);
         H = N/60;
         M = N%60;
-        System.out.println(H+" "+M);
+        System.out.println(H+" "+M); // displaying output
         input.close();
     }
 }
