56435d44e7ed2d192ea0799c7868ffe27f986e9f
albashzia <ahmedalbashzia@gmail.com>
2026-01-31 21:21:55 +0500

    Practicing the printing of character at a specific index

--- Strings/StringBuilderPractice.java
+++ Strings/StringBuilderPractice.java
@@ -3,5 +3,9 @@ import java.lang.StringBuilder;
 public class StringBuilderPractice {
     public static void main(String[] args) {
         StringBuilder sb = new StringBuilder("StringBuilder Practice");
+
+        //printing character at a specific index
+        System.out.println(sb.charAt(0));
+        System.out.println(sb.charAt(8));
     }
 }
