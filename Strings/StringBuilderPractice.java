7ecc4b6ad7f1f116f437b85b2f96ccb823454dd8
albashzia <ahmedalbashzia@gmail.com>
2026-01-31 20:41:41 +0500

    Creating StringBuilderPractice.java to practice concepts of string builder

---
+++ Strings/StringBuilderPractice.java
@@ -0,0 +1,7 @@
+package Strings;
+import java.lang.StringBuilder;
+public class StringBuilderPractice {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("StringBuilder Practice");
+    }
+}
