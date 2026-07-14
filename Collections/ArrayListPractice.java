b3abcc94d7430ec5eadf738f15f803ded212f301
albashzia <ahmedalbashzia@gmail.com>
2026-07-14 16:38:58 +0500

    Add elements to array list and print them using get method and for-each loop

--- Collections/ArrayListPractice.java
+++ Collections/ArrayListPractice.java
@@ -6,5 +6,15 @@ import java.util.List;
 public class ArrayListPractice {
     public static void main(String[] args){
         List<Integer> list = new ArrayList<>();
+
+        list.add(1);
+        list.add(10);
+        list.add(100);
+
+        list.get(1);
+
+        for(int x : list){
+            System.out.println(x);
+        }
     }
 }
