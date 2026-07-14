c3438f0c61ed326916cde8115695a03d36e050f2
albashzia <ahmedalbashzia@gmail.com>
2026-07-14 16:37:29 +0500

    Add main method and define an ArrayList of Integer data type

--- Collections/ArrayListPractice.java
+++ Collections/ArrayListPractice.java
@@ -1,4 +1,10 @@
 package Collections;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ArrayListPractice {
+    public static void main(String[] args){
+        List<Integer> list = new ArrayList<>();
+    }
 }
