6614aebc14d766655ccf7deb180b3717c267b294
albashzia <ahmedalbashzia@gmail.com>
2026-07-22 16:37:03 +0500

    Create a new class named StringLengthComparator inside ComparatorPractice.java file and implement Comparator interface along with overridden methods

--- Collections/ComparatorPractice.java
+++ Collections/ComparatorPractice.java
@@ -1,4 +1,14 @@
 package Collections;
 
+
+import java.util.Comparator;
+
+class StringLengthComparator implements Comparator<String>{
+
+    @Override
+    public int compare(String o1, String o2) {
+        return 0;
+    }
+}
 public class ComparatorPractice {
 }
