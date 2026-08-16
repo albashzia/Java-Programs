ab7bc104e3a1a927d1b4b50bf6bd3627ad43c8f5
albashzia <ahmedalbashzia@gmail.com>
2026-08-16 23:12:30 +0500

    Create a linked list in main method of LinkedListPractice class and add some values to it

--- Collections/LinkedListPractice.java
+++ Collections/LinkedListPractice.java
@@ -1,7 +1,12 @@
 package Collections;
 
+import java.util.LinkedList;
+
 public class LinkedListPractice {
     public static void main() {
-
+        LinkedList<Integer> integerLinkedList = new LinkedList<>();
+        integerLinkedList.add(1);
+        integerLinkedList.add(2);
+        integerLinkedList.add(3);
     }
 }
