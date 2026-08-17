08f96c661c7a715f02f5a6d59432c7f1e66ef924
albashzia <ahmedalbashzia@gmail.com>
2026-08-17 20:21:03 +0500

    Add data to the vector collection object and print its capacity before and after capacity's increment

--- Collections/VectorPractice.java
+++ Collections/VectorPractice.java
@@ -5,6 +5,13 @@ import java.util.Vector;
 public class VectorPractice {
     public static void main() {
         Vector<Integer> vector = new Vector<>(5,3);
-
+        vector.add(1);
+        vector.add(1);
+        vector.add(1);
+        vector.add(1);
+        vector.add(1);
+        System.out.println(vector.capacity());
+        vector.add(1);
+        System.out.println(vector.capacity());
     }
 }
