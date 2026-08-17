b39b985ff0317dc9cecbcedc96f782858dce549b
albashzia <ahmedalbashzia@gmail.com>
2026-08-17 22:34:10 +0500

    Push more elements to the stack and use peek method to view the top most element of the stack

--- Collections/StackPractice.java
+++ Collections/StackPractice.java
@@ -7,8 +7,13 @@ public class StackPractice {
         Stack<Integer> integerStack = new Stack<>();
         integerStack.push(1);
         integerStack.push(2);
+        integerStack.push(3);
+        integerStack.push(4);
+        integerStack.push(5);
         System.out.println(integerStack);
         integerStack.pop();
         System.out.println(integerStack);
+
+        System.out.println(integerStack.peek());
     }
 }
