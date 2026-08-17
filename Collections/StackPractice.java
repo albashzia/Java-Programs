2e46a59b839cd9fb81337625a51c766813fe9da7
albashzia <ahmedalbashzia@gmail.com>
2026-08-17 22:31:28 +0500

    Push some elements inside the stack and print the stack

--- Collections/StackPractice.java
+++ Collections/StackPractice.java
@@ -5,5 +5,8 @@ import java.util.Stack;
 public class StackPractice {
     public static void main(String[] args){
         Stack<Integer> integerStack = new Stack<>();
+        integerStack.push(1);
+        integerStack.push(2);
+        System.out.println(integerStack);
     }
 }
