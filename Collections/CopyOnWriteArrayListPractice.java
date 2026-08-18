bb42d2fa1730e61ce37db08be45364968233db16
albashzia <ahmedalbashzia@gmail.com>
2026-08-18 22:58:44 +0500

    Add elements to CopyOnWriteArrayList holding integers

--- Collections/CopyOnWriteArrayListPractice.java
+++ Collections/CopyOnWriteArrayListPractice.java
@@ -5,5 +5,9 @@ import java.util.concurrent.CopyOnWriteArrayList;
 public class CopyOnWriteArrayListPractice {
     public static void main(String[] args) {
         CopyOnWriteArrayList<Integer> integers = new CopyOnWriteArrayList<>();
+
+        integers.add(1);
+        integers.add(2);
+        integers.add(3);
     }
 }
