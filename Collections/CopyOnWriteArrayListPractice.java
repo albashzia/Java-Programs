ab3aafad3f6011ad57ce6a483bae27b294f0b714
albashzia <ahmedalbashzia@gmail.com>
2026-08-18 23:02:48 +0500

    Print the CopyOnWriteArrayList before reading, perform an operation while reading and print the updated CopyOnWriteArrayList afterward

--- Collections/CopyOnWriteArrayListPractice.java
+++ Collections/CopyOnWriteArrayListPractice.java
@@ -9,5 +9,16 @@ public class CopyOnWriteArrayListPractice {
         integers.add(1);
         integers.add(2);
         integers.add(3);
+
+        System.out.println("Initial List: "+integers);
+        for (int integer: integers){
+            System.out.println(integer);
+            if (integer == 3){
+                integers.add(4);
+                System.out.println("Added 4 while reading");
+            }
+        }
+
+        System.out.println("Updated List: "+integers);
     }
 }
