b2e2269050c2e6890e2dbec65a185368348ec7fb
albashzia <ahmedalbashzia@gmail.com>
2026-08-18 22:34:44 +0500

    Create a CopyOnWriteArrayList inside the main method to hold integer objects

--- Collections/CopyOnWriteArrayListPractice.java
+++ Collections/CopyOnWriteArrayListPractice.java
@@ -1,7 +1,9 @@
 package Collections;
 
+import java.util.concurrent.CopyOnWriteArrayList;
+
 public class CopyOnWriteArrayListPractice {
     public static void main(String[] args) {
-
+        CopyOnWriteArrayList<Integer> integers = new CopyOnWriteArrayList<>();
     }
 }
