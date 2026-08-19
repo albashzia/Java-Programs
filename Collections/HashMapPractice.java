ed62fde079f59ba96ab5f92804a1d03cb47e062f
albashzia <ahmedalbashzia@gmail.com>
2026-08-19 21:42:35 +0500

    Add some values to the hashMap using Key-Value pairs and print the hashMap

--- Collections/HashMapPractice.java
+++ Collections/HashMapPractice.java
@@ -5,5 +5,11 @@ import java.util.HashMap;
 public class HashMapPractice {
     public static void main(String[] args){
         HashMap<Integer, String> hashMap = new HashMap<>();
+
+        hashMap.put(1,"Jack");
+        hashMap.put(2,"James");
+        hashMap.put(3,"John");
+
+        System.out.println(hashMap);
     }
 }
