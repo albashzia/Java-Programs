6c7daa976ad613aa3c65c812ff761e9d03bca5e3
albashzia <ahmedalbashzia@gmail.com>
2026-08-19 21:28:06 +0500

    Add main method to HashMapPractice class and create a HashMap object

--- Collections/HashMapPractice.java
+++ Collections/HashMapPractice.java
@@ -1,4 +1,9 @@
 package Collections;
 
+import java.util.HashMap;
+
 public class HashMapPractice {
+    public static void main(String[] args){
+        HashMap hashMap = new HashMap<>();
+    }
 }
