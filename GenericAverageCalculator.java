74aa5b776ac388971744833501a60283c5c8c42a
albashzia <ahmedalbashzia@gmail.com>
2026-09-02 21:33:35 +0500

    Add calculateAverage method to generically calculate the average

--- GenericAverageCalculator.java
+++ GenericAverageCalculator.java
@@ -1,2 +1,10 @@
 public class GenericAverageCalculator {
+    public static <T extends Number> double calculateAverage(T[] array){
+        double sum = 0;
+        for (T number : array) {
+            sum += number.doubleValue();
+        }
+        return sum / array.length;
+    }
+
 }
