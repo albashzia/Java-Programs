3245821f1a712f0e073e0bea13e628160ee3994d
albashzia <ahmedalbashzia@gmail.com>
2026-09-03 20:24:30 +0500

    Add main method to demonstrate multiply with mixed numeric types

--- GenericNumberMultiplier.java
+++ GenericNumberMultiplier.java
@@ -3,4 +3,11 @@ public class GenericNumberMultiplier {
         return a.doubleValue() * b.doubleValue();
     }
 
+    public static void main(String[] args) {
+        Integer num1 = 5;
+        Double num2 = 3.5;
+
+        double result = multiply(num1, num2);
+        System.out.println("Multiplication Result: " + result);
+    }
 }
