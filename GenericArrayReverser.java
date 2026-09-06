78cc0a286ca240950be8800aade1eeb4abdbfca0
albashzia <ahmedalbashzia@gmail.com>
2026-09-06 19:15:00 +0500

    Implement generic reverseArray method for Number types

--- GenericArrayReverser.java
+++ GenericArrayReverser.java
@@ -1,2 +1,14 @@
 public class GenericArrayReverser {
+
+    public static <T extends Number> void reverseArray(T[] array) {
+        int left = 0;
+        int right = array.length - 1;
+        while (left < right) {
+            T temp = array[left];
+            array[left] = array[right];
+            array[right] = temp;
+            left++;
+            right--;
+        }
+    }
 }
