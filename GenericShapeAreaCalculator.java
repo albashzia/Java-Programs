436f9bcee2593c342f16268f545d19cd2cf3830d
albashzia <ahmedalbashzia@gmail.com>
2026-09-07 16:35:17 +0500

    Create a Circle class which extends Shape class and overrides the getArea method of Shape class

--- GenericShapeAreaCalculator.java
+++ GenericShapeAreaCalculator.java
@@ -3,5 +3,18 @@ abstract class Shape {
 }
 
 
+class Circle extends Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+}
+
 public class GenericShapeAreaCalculator {
 }
