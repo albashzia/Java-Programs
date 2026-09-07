f004285e0801c01f69bb92933a22242317d03d93
albashzia <ahmedalbashzia@gmail.com>
2026-09-07 16:36:16 +0500

    Add Rectangle class extending Shape class and overriding the getArea method of Shape class

--- GenericShapeAreaCalculator.java
+++ GenericShapeAreaCalculator.java
@@ -16,5 +16,20 @@ class Circle extends Shape {
     }
 }
 
+class Rectangle extends Shape {
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public double getArea() {
+        return width * height;
+    }
+}
+
 public class GenericShapeAreaCalculator {
 }
